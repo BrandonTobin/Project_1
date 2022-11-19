e400eea48fbf76a085712e4284c2846d48ef77ef
Brandon Tobin <brandontobinbusiness@gmail.com>
2022-11-19 16:32:24 -0600

    Added animalNames.push to get attribute animals on click

--- game.js
+++ game.js
@@ -2,17 +2,23 @@ let clickedArray = []
 let firstSelection = ''
 let secondSelection = ''
 
-function doesMatch() {
-  clickedArray.forEach((clickedCard) => {})
+function doesMatch(arr) {
+  animalNames = []
+  arr.forEach((clickedCard) => {
+    animalNames.push(clickedCard.getAttribute('animal'))
+  })
+  animalNames.unique()
+  console.log(animalNames.unique())
 }
 
 const cards = document.querySelectorAll('.cards .card')
 cards.forEach((card) => {
   card.addEventListener('click', () => {
     card.classList.add('clicked')
+    console.log(card)
     clickedArray.push(card)
     if (clickedArray.length % 2 == 0) {
-      doesMatch()
+      doesMatch(clickedArray)
     }
   })
 })
