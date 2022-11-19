f4f071bc7fb31d698e134fc42197d8b0f829178d
Brandon Tobin <brandontobinbusiness@gmail.com>
2022-11-19 16:11:24 -0600

    clicked card to reveal opacity from 0 to 1, alert says its a match

---
+++ README.md
@@ -0,0 +1,7 @@
+click card and make visible (animation ?)
+
+if 2 cards are selected and not matching, revert back to hidden
+
+if 2 cards are matching, keep visible
+
+1 minute timer, if timer finishes you lose
--- game.js
+++ game.js
@@ -1,11 +1,18 @@
-let counter = 0
+let clickedArray = []
 let firstSelection = ''
 let secondSelection = ''
+
+function doesMatch() {
+  clickedArray.forEach((clickedCard) => {})
+}
 
 const cards = document.querySelectorAll('.cards .card')
 cards.forEach((card) => {
   card.addEventListener('click', () => {
     card.classList.add('clicked')
-    console.log('clicked')
+    clickedArray.push(card)
+    if (clickedArray.length % 2 == 0) {
+      doesMatch()
+    }
   })
 })
