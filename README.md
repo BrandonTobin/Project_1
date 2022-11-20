5b495bb11dc57a084d170597e314f85c93cf7cf2
Brandon Tobin <brandontobinbusiness@gmail.com>
2022-11-19 18:43:03 -0600

    Added timer

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-click card and make visible (animation ?)
+~~click card and make visible (animation ?)~~
 
-if 2 cards are selected and not matching, revert back to hidden
+~~if 2 cards are selected and not matching, revert back to hidden~~
 
-if 2 cards are matching, keep visible
+~~if 2 cards are matching, keep visible~~
 
 1 minute timer, if timer finishes you lose
+
+maybe freeze the ui while the game is checking if a there is a match
--- game.js
+++ game.js
@@ -1,24 +1,47 @@
 let clickedArray = []
-let firstSelection = ''
-let secondSelection = ''
 
 function doesMatch(arr) {
-  animalNames = []
+  let animalNames = []
+
   arr.forEach((clickedCard) => {
     animalNames.push(clickedCard.getAttribute('animal'))
   })
-  uniqueArray = animalNames.filter(function (item, pos) {
+
+  let uniqueArray = animalNames.filter(function (item, pos) {
     return animalNames.indexOf(item) == pos
   })
+
   return uniqueArray.length == 1
 }
 
+function startTimer() {
+  let seconds = 2
+
+  setInterval(() => {
+    seconds--
+
+    if (seconds >= 0) {
+      let secondsElement = document.querySelector('.seconds')
+      secondsElement.innerHTML = seconds
+    }
+  }, 1000)
+}
+
 const cards = document.querySelectorAll('.cards .card')
+
+document.addEventListener(
+  'click',
+  () => {
+    startTimer()
+  },
+  { once: true }
+)
+
 cards.forEach((card) => {
   card.addEventListener('click', () => {
     card.classList.add('clicked')
-    console.log(card)
     clickedArray.push(card)
+
     if (clickedArray.length % 2 == 0) {
       if (doesMatch(clickedArray)) {
         clickedArray.forEach((card) => {
@@ -26,8 +49,14 @@ cards.forEach((card) => {
           card.classList.toggle('clicked')
           clickedArray = []
         })
+      } else {
+        clickedArray.forEach((card) => {
+          setTimeout(() => {
+            card.classList.toggle('clicked')
+            clickedArray = []
+          }, 500)
+        })
       }
-      console.log(doesMatch(clickedArray))
     }
   })
 })
